ce01fba9fd3099c3baa43b06f919b36b661b2db7
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-10-01 14:25:51 +0300

    ADD: init arch

---
+++ server.go
@@ -0,0 +1,27 @@
+package testeffectivemobilego2024
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, rtm, wtm time.Duration, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 20, // 1MB
+		ReadTimeout:    rtm * time.Second,
+		WriteTimeout:   wtm * time.Second,
+	}
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
