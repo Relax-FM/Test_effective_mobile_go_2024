bdf2ec3b0d74f45154c59497f7934799ab02a197
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-10-02 18:27:55 +0300

    FIX: delete handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -27,8 +27,8 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		music := api.Group("/music")
 		{
 			music.GET("/", h.getAllMusic) // 
-			music.GET("/:id", h.getMusicText) //
-			music.DELETE("/:id", h.deleteMusicItem) //
+			music.GET("/:id", h.getMusicText) // 
+			music.DELETE("/:id", h.deleteMusicItem) // Готово
 			music.PUT("/:id", h.updateMusicItem) // Готово
 			music.POST("/", h.addMusicItem) // Готово
 		}
