000e73476d03c22886941d7426b40ceeaf17a26f
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-10-06 22:38:25 +0300

    ADD: some comments (test doc comments)

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -20,6 +20,7 @@ type Config struct {
 	SSLMode  string
 }
 
+// Create new connection for DB
 func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
 	if err != nil {
